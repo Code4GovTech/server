ed35bc492ed2e686d2a12c08d0e544c6a73bb1b8
sasi <sasikumar.s@cogniverselabs.com>
2024-03-20 18:40:48 +0530

    keys removed

--- utils/connect_db.py
+++ utils/connect_db.py
@@ -8,7 +8,6 @@ def connect_db():
     password = os.getenv('DB_PASSWORD')
     
     conn_str = f"dbname='{dbname}' user='{user}' host='{host}' password='{password}'"
-    # conn = psycopg2.connect("dbname='postgres' user='postgres.kcavhjwafgtoqkqbbqrd' host='aws-0-ap-south-1.pooler.supabase.com' password='2Vts525G4cvEiQhK'")   
     conn = psycopg2.connect(conn_str)
     cur = conn.cursor()
     return conn, cur
