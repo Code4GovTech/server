4fbd892af83115de9827b8cdff0a6bdd885ae5b4
jaanbaaz <jaanbaaz.akhtar@esmagico.in>
2024-08-14 18:11:56 +0530

    issue_comment processing

--- handlers/issue_comment_handler.py
+++ handlers/issue_comment_handler.py
@@ -31,4 +31,60 @@ class Issue_commentHandler(EventHandler):
             logger.info(f"{datetime.now()}---{e}")
             raise Exception 
         
+    async def handle_issue_comment_created(self, data, supabase_client):
+        try:        
+            #generate sample dict for ticket comment table
+            comment_data = {
+                'url':data['issue']['comments_url'],
+                'html_url':data['issue']['html_url'],
+                'issue_url':data['comment']['issue_url'],
+                'node_id':data['issue']['node_id'],
+                'commented_by':data['comment']['user']['login'],
+                'commented_by_id':data['comment']['user']['id'],
+                'content':data['comment']['body'],
+                'reactions_url':data['comment']['reactions']['url'],
+                'ticket_url':data['issue']['url'],
+                'id':data['comment']['id'],
+                'created_at':str(datetime.now()),
+                'updated_at':str(datetime.now())
+                
+            }
+            
+            save_data = supabase_client.add_data(comment_data,"ticket_comments")            
+            if save_data == None:
+                logger.info(f"{datetime.now()}--- Failed to save data in ticket_comments")
+                     
+        except Exception as e:
+            logger.info(f"{datetime.now()}---{e}")
+            raise Exception 
+        
+    
+    async def handle_issue_comment_edited(self, data, supabase_client):
+        try:        
+            #generate sample dict for ticket comment table
+            comment_data = {
+                'url':data['issue']['comments_url'],
+                'html_url':data['issue']['html_url'],
+                'issue_url':data['comment']['issue_url'],
+                'node_id':data['issue']['node_id'],
+                'commented_by':data['comment']['user']['login'],
+                'commented_by_id':data['comment']['user']['id'],
+                'content':data['comment']['body'],
+                'reactions_url':data['comment']['reactions']['url'],
+                'ticket_url':data['issue']['url'],
+                'id':data['comment']['id'],
+                'created_at':str(datetime.now()),
+                'updated_at':str(datetime.now())
+                
+            }
+            
+            save_data = supabase_client.add_data(comment_data,"ticket_comments")            
+            if save_data == None:
+                logger.info(f"{datetime.now()}--- Failed to save data in ticket_comments")
+                     
+        except Exception as e:
+            logger.info(f"{datetime.now()}---{e}")
+            raise Exception 
+
+        
         
