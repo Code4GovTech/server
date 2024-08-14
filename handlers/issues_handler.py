29a3d0b1733f6d1f05088d75046df62a7343247e
sasi <sasikumar.s@cogniverselabs.com>
2024-08-14 21:28:56 +0530

    postgres ORM with models

--- handlers/issues_handler.py
+++ handlers/issues_handler.py
@@ -3,15 +3,15 @@ from events.ticketEventHandler import TicketEventHandler
 from handlers.EventHandler import EventHandler
 
 class IssuesHandler(EventHandler):
-    async def handle_event(self, data, supabase_client):
+    async def handle_event(self, data, postgres_client):
         # Implement your logic for handling issue events here
         try:        
             if data.get("issue"):
                 issue = data["issue"]
-                if supabase_client.checkUnlisted(issue["id"]):
-                    supabase_client.deleteUnlistedTicket(issue["id"])
+                if await postgres_client.check_record_exists("unlisted_tickets","issue_id",issue["id"]):
+                    await postgres_client.delete("unlisted_tickets","issue_id",issue["id"])
                 await TicketEventHandler().onTicketCreate(data)
-                if supabase_client.checkIsTicket(issue["id"]):
+                if await postgres_client.checkIsTicket("unlisted_tickets","issue_id",issue["id"]):
                     await TicketEventHandler().onTicketEdit(data)
                     if data["action"] == "closed":
                         await TicketEventHandler().onTicketClose(data)
