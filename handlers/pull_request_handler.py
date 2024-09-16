d08fa2816c420f199027df2f4c819286f2a5c2aa
jaanbaaz <jaanbaaz.akhtar@esmagico.in>
2024-09-16 17:10:54 +0530

    logs added for debugging

--- handlers/pull_request_handler.py
+++ handlers/pull_request_handler.py
@@ -5,7 +5,8 @@ from handlers.EventHandler import EventHandler
 class Pull_requestHandler(EventHandler):
     async def handle_event(self, data, postgres_client):
         # Implement your logic for handling issue events here
-        try:            
+        try:
+            print('inside pull request handler ', data)            
             pr_data = {
                 "created_at": data['pull_request']['created_at'],
                 "api_url":None,
@@ -27,19 +28,22 @@ class Pull_requestHandler(EventHandler):
             points = await postgres_client.get_data("url","ccbp_tickets",data['pull_request']['issue_url'],None)  
             pr_data['points'] = points[0]['ticket_points'] if points else 0
             pr_data['ticket_complexity'] = points[0]['complexity'] if points else None
+
+            print('PR data ', pr_data)
             
-            save_data = await postgres_client.add_data(pr_data,"pr_history")            
+            save_data = await postgres_client.add_data(pr_data,"pr_history")
+            print('saved data in PR ', save_data)            
             if save_data == None:
                 logging.info("Failed to save data in pr_history")
 
             user_id = data['pull_request']['user']['id']
 
             #get contributor_id and save to supabase
-            contributor = supabase_client.get_data('github_id', 'contributors_registration', user_id, '*')
+            contributor = await postgres_client.get_data('github_id', 'contributors_registration', user_id, '*')
             contributor_id = contributor["id"]
 
             issue_url = data['pull_request']['issue_url']
-            issue = supabase_client.get_data('issue_url', 'issues', issue_url, '*')
+            issue = await postgres_client.get_data('issue_url', 'issues', issue_url, '*')
 
             #save activity to user_activity
             activity_data = {
@@ -50,7 +54,7 @@ class Pull_requestHandler(EventHandler):
                 "contributor_id": contributor_id,
                 "mentor_id": ""
             }
-            saved_activity_data = supabase_client.add_data(activity_data,"user_activity")
+            saved_activity_data = await postgres_client.add_data(activity_data,"user_activity")
         except Exception as e:
             logging.info(e)
             raise Exception
